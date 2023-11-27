84b384db85cb6731223ce4880f5d012679fb4de3
tokokitabutsuri <136794662+tokokitabutsuri@users.noreply.github.com>
2023-11-27 01:55:14 +0000

    add supabase init

---
+++ api/article_update_for_db.mjs
@@ -0,0 +1,4 @@
+import { createClient } from '@supabase/supabase-js'
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY)
