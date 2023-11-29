a4bc332a331d258a82a0129eb4f4fb3518777cda
tokokitabutsuri <136794662+tokokitabutsuri@users.noreply.github.com>
2023-11-29 11:50:33 +0900

    fix

--- articles/article.js
+++ articles/article.js
@@ -1,18 +1,16 @@
-try {
-    window.alert("article.js");
-    const { createClient } = microcms;
 
-    const client = createClient({
-        serviceDomain: 'tkbutsuribu', // service-domain は XXXX.microcms.io の XXXX 部分
-        apiKey: '41k5Ew3OXIRepVdY8CgIXiAwTNwJiS5mQFpa',
-    })
+const { createClient } = microcms;
 
+const client = createClient({
+    serviceDomain: 'tkbutsuribu', // service-domain は XXXX.microcms.io の XXXX 部分
+    apiKey: '41k5Ew3OXIRepVdY8CgIXiAwTNwJiS5mQFpa',
+})
 
-    let contentID = 'n39zOq9PAW'
+const updateArticle = (id) => {
     client
         .get({
             endpoint: 'articles',
-            contentId: contentID,
+            contentId: id,
         })
         .then((res) => {
             window.alert(res.title)
@@ -20,11 +18,8 @@ try {
         })
 
 
-
-} catch (e) {
-    window.alert(e);
 }
 
 //Prism.highlightAll();
 //で読み込む
-
+window.addEventListener('DOMContentLoaded',updateArticle("n39zOq9PAW"));
