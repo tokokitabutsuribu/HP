d927e9d0179241902ecc0187a91c2184e6a162d6
tokokitabutsuri <136794662+tokokitabutsuri@users.noreply.github.com>
2023-11-29 11:46:44 +0900

    debug

--- articles/article.js
+++ articles/article.js
@@ -1,25 +1,27 @@
-const { createClient } = microcms;
+try {
+    window.alert("article.js");
+    const { createClient } = microcms;
 
-const client = createClient({
-    serviceDomain: 'tkbutsuribu', // service-domain は XXXX.microcms.io の XXXX 部分
-    apiKey: '41k5Ew3OXIRepVdY8CgIXiAwTNwJiS5mQFpa',
-})
-
-try{
-let contentID = 'n39zOq9PAW'
-client
-    .get({
-        endpoint: 'articles',
-        contentId: contentID,
-    })
-    .then((res) => {
-        window.alert(res.title)
-        document.querySelector('#title').textContent = res.title;
+    const client = createClient({
+        serviceDomain: 'tkbutsuribu', // service-domain は XXXX.microcms.io の XXXX 部分
+        apiKey: '41k5Ew3OXIRepVdY8CgIXiAwTNwJiS5mQFpa',
     })
 
 
+    let contentID = 'n39zOq9PAW'
+    client
+        .get({
+            endpoint: 'articles',
+            contentId: contentID,
+        })
+        .then((res) => {
+            window.alert(res.title)
+            document.querySelector('#title').textContent = res.title;
+        })
 
-}catch(e){
+
+
+} catch (e) {
     window.alert(e);
 }
 
