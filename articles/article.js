9204bfe980b51f541d2061103eedaa1ab821ed46
tokokitabutsuri <136794662+tokokitabutsuri@users.noreply.github.com>
2023-11-29 12:35:45 +0900

    update

--- articles/article.js
+++ articles/article.js
@@ -5,43 +5,45 @@ const client = createClient({
     serviceDomain: 'tkbutsuribu', // service-domain は XXXX.microcms.io の XXXX 部分
     apiKey: '41k5Ew3OXIRepVdY8CgIXiAwTNwJiS5mQFpa',
 })
-try{
-const addarray = (id, array, tag) => {
-    let add = "";
-    let starttag;
-    let endtag;
-    if (tag !== undefined) {
-        starttag = '<' + tag + '>'
-        endtag = '</' + tag + '>\n'
-    } else {
-        starttag = ''
-        endtag = ''
+try {
+    const addarray = (id, array, tag) => {
+        let add = "";
+        let starttag;
+        let endtag;
+        if (tag !== undefined) {
+            starttag = '<' + tag + '>'
+            endtag = '</' + tag + '>\n'
+        } else {
+            starttag = ''
+            endtag = ''
+        }
+        if (!array.lenth) {
+            for (const elem of array) {
+                add += starttag + elem + endtag;
+            }
+        }
+        document.querySelector(id).innerHTML = add;
     }
-    for (const elem of array) {
-        add += starttag + elem + endtag;
+
+    const updateArticle = (id) => {
+        client
+            .get({
+                endpoint: 'articles',
+                contentId: 'pzazaa-hz8',
+            })
+            .then((res) => {
+                addarray('#category', res.category, 'li')
+                document.querySelector('#title').textContent = res.title;
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+
     }
-    document.querySelector(id).innerHTML = add;
-}
 
-const updateArticle = (id) => {
-    client
-        .get({
-            endpoint: 'articles',
-            contentId: 'pzazaa-hz8',
-        })
-        .then((res) => {
-            addarray('#category', res.category, 'li')
-            document.querySelector('#title').textContent = res.title;
-        })
-        .catch((error) => {
-            console.log(error)
-        })
-
-}
-
-//Prism.highlightAll();
-//で読み込む
-window.addEventListener('DOMContentLoaded', updateArticle());
-}catch(e){
+    //Prism.highlightAll();
+    //で読み込む
+    window.addEventListener('DOMContentLoaded', updateArticle());
+} catch (e) {
     window.alert(e);
 }
