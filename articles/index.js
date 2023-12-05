1a04f5140ee817b0f217fb151d4e826eb654a14e
tokokitabutsuri <136794662+tokokitabutsuri@users.noreply.github.com>
2023-12-05 15:59:57 +0900

    update

--- articles/index.js
+++ articles/index.js
@@ -1,13 +1,48 @@
+var page;
+const Initialize = () => {
+    const searchParams = new URLSearchParams(window.location.search);
+    if (searchParams.has('page')) {
+        page = parseInt(searchParams.get("page"));
+        if (page == '') {
+            page = 1;
+        }
+    } else {
+        page = 1;
+    }
+    document.getElementById('pagenum').innerText = page;
 
+    if (page <= 1) {
+        document.getElementById('pageminus').style.visibility = 'hidden';
+    }
+}
 
-const updateindex=()=>{
+const getArticledata = () => {
 
 }
 
+const updateDOM = () => {
 
+}
 
+const pageplus = () => {
+    window.location.href = '/index.html?page=' + (page + 1);
+}
 
+const pageminus = () => {
+    if (page > 2) {
+        window.location.href = '/index.html?page=' + (page - 1);
+    }
+}
 
-window.addEventListener('DOMContentLoaded', {
-
-})
+window.addEventListener('DOMContentLoaded', async () => {
+    const header = fetch("https://tkbutsuribu.vercel.app/header.html")
+        .then((response) => response.text())
+        .then((data) => document.querySelector("#header").innerHTML = data);
+    const footer = fetch("https://tkbutsuribu.vercel.app/footer.html")
+        .then((response) => response.text())
+        .then((data) => document.querySelector("#footer").innerHTML = data);
+    const getArticle = getArticledata();
+    const init = Initialize();
+    await Promise.all([header, footer, getArticle, init]);
+    await updateDOM();
+});
