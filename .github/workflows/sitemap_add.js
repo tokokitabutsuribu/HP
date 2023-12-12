75bdeb1799abd68e17b6b89ad7430b899547f8ce
tokokitabutsuri <136794662+tokokitabutsuri@users.noreply.github.com>
2023-12-12 16:26:15 +0900

    update GA

---
+++ .github/workflows/sitemap_add.js
@@ -0,0 +1,48 @@
+import fetchBooks from './fetchBooks'
+// xml 生成には xml2js というライブラリを使っています
+import { Builder } from 'xml2js';
+const xpath = require('xpath');
+const dom = require('xmldom').DOMParser;
+const fs = require('fs');
+
+try {
+
+  const xml = fs.readFileSync('/sitemap.xml', "utf-8");
+  const doc = new dom().parseFromString(xml);
+  const select = xpath.useNamespaces({
+    "a": "http://www.sitemaps.org/schemas/sitemap/0.9"
+  });
+
+  // xml に記述する URL のリスト作成
+  const urls = []
+
+  const oldurls = select("/a:urlse/a:url", doc);
+
+  for (const oldurl of oldurls) {
+    console.log(oldurl)
+    //const name = select("a:loc/text()", oldurl);
+    urls.push(oldurl);
+  }
+
+  console.log(process.argv[2]);
+  urls.push({ loc: "https://tkbutsuribu.vercel.app/articles/article.html?id=" + process.argv[2] })
+
+  // xml 生成
+  console.log(urls)
+  const builder = new Builder();
+  const sitemap = {
+    urlset: {
+      $: {
+        xmlns: 'http://www.sitemaps.org/schemas/sitemap/0.9',
+      },
+      url: urls,
+    },
+  };
+  const xml = builder.buildObject(sitemap);
+
+  fs.writeFile('/sitemap.xml', xml);
+} catch (e) {
+  console.error(e);
+  process.exit(1);
+}
+
