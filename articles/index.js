aecfa158dd8e81efedd953029096258044c88c39
所北物理部 <136794662+tokokitabutsuri@users.noreply.github.com>
2023-12-18 17:25:47 +0900

    fix

--- articles/index.js
+++ articles/index.js
@@ -25,7 +25,7 @@ try {
         } else {
             page = 1;
         }
-        window.location.replace = window.location.origin + '/articles/index.html?page=' + page;
+        window.location.replace(window.location.origin + '/articles/index.html?page=' + page);
         document.getElementById('pagenum').innerText = page;
 
         if (page <= 1) {
