6452d26d40d00e4b3cb9f8faeab91112f9754eaf
所北物理部 <136794662+tokokitabutsuri@users.noreply.github.com>
2023-12-27 00:53:29 +0900

    update 404 article

--- articles/article.js
+++ articles/article.js
@@ -6,15 +6,15 @@ const client = createClient({
     apiKey: '41k5Ew3OXIRepVdY8CgIXiAwTNwJiS5mQFpa',
 })
 try {
-    const replacecontent = (input)=>{
+    const replacecontent = (input) => {
         const replacekeys = [
-            {key:'<table>',replace:'<div class="table-wrapper"><table>'},
-            {key:'</table>',replace:'</table></div>'},
-            {key:'<iframe',replace:'<iframe loading="lazy"'},
+            { key: '<table>', replace: '<div class="table-wrapper"><table>' },
+            { key: '</table>', replace: '</table></div>' },
+            { key: '<iframe', replace: '<iframe loading="lazy"' },
         ]
         let ret = input;
-        for(const replacekey of replacekeys){
-            ret = ret.replaceAll(replacekey.key,replacekey.replace);
+        for (const replacekey of replacekeys) {
+            ret = ret.replaceAll(replacekey.key, replacekey.replace);
         }
         return ret;
     }
@@ -71,17 +71,22 @@ try {
                 .then((response) => response.text())
                 .then(text => new DOMParser().parseFromString(text, "text/html").body.innerHTML)
                 .then((data) => document.querySelector("body").innerHTML = data);
+            document.title = "ページないよ -所北物理部";
+            document.querySelector('meta[name="description"]').setAttribute("content", "お探しの記事は見つかりませんでした。URLが間違っているか、削除された可能性があります。");
+            document.querySelector('meta[property="og:url"]').setAttribute("content", "https://tkbutsuribu.vercel.app/articles/article.html?id=" + id);
+            document.querySelector('meta[property="og:title"]').setAttribute("content", "ページないよ");
+            document.querySelector('meta[property="og:description"]').setAttribute("content", "お探しの記事は見つかりませんでした");
             return
         }
         const res = articledata;
         addarray('#category', res.category, 'li', undefined)
         document.querySelector('#title').textContent = res.title;
         document.querySelector('#updated').textContent = new Date(res.revisedAt).toISOString().split("T")[0].replaceAll("-", "/");
-        try{
-        addarray('#content', res.contents, undefined, 'content');
-    }catch(e){
-        console.log(e);
-    }
+        try {
+            addarray('#content', res.contents, undefined, 'content');
+        } catch (e) {
+            console.log(e);
+        }
         let add = ""
         for (const elem of res.index) {
             add += '<li><a href="javascript:window.location.replace(\'#' + elem.link + '\')">' + elem.index + '</a></li>\n';
