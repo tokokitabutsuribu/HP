c953a51d964ca5098dadcae7755f1132b67e33a7
tokokitabutsuri <136794662+tokokitabutsuri@users.noreply.github.com>
2024-01-18 03:09:45 +0900

    update

--- app/api/test/test.js
+++ app/api/test/test.js
@@ -1,5 +1,5 @@
-function test(req) {
-  return new Response({ message: 'success' }, { status: 200 });
+function test(req,res) {
+  return res.status(200).json({ "status": 'success' });
 }
 
 export { test };
