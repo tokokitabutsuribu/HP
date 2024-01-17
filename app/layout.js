9da5567763a2ff52f2b93fbc141529f67620e5fa
所北物理部 <136794662+tokokitabutsuri@users.noreply.github.com>
2024-01-17 09:21:54 +0900

    add meticulous

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,19 @@
 export default function RootLayout({
-    children,
-  }) {
-    return (
-      <html lang="ja">
-        <body>{children}</body>
-      </html>
-    );
-  }
+  children,
+}) {
+  return (
+    <html lang="ja">
+      <head>
+        {(process.env.NODE_ENV === "development" || process.env.VERCEL_ENV === "preview") && (
+          // eslint-disable-next-line @next/next/no-sync-scripts
+          <script
+            data-project-id="cy4LiIr4fnVnIeHpS860b4iP9OxJkgyFFyYe6grP"
+            data-is-production-environment="false"
+            src="https://snippet.meticulous.ai/v1/meticulous.js"
+          />
+        )}
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
