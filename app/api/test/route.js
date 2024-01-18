5964c66992122dd85c9f6dd165eb531c35f223bb
tokokitabutsuri <136794662+tokokitabutsuri@users.noreply.github.com>
2024-01-19 02:04:43 +0900

    test

--- app/api/test/route.js
+++ app/api/test/route.js
@@ -1,2 +1,7 @@
-import { test } from './test';
-export { test as GET };
+//import { test } from './test';
+//export { test as GET };
+export default function handler(req, res) {
+    res.status = 200;
+    return res.send(JSON.stringify({ message: 'success' }));
+
+}
