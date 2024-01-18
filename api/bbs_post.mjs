85b42750a2562d8df76aeb624a6fed578b0f3c18
tokokitabutsuri <136794662+tokokitabutsuri@users.noreply.github.com>
2024-01-19 01:48:03 +0900

    remove test

--- api/bbs_post.mjs
+++
@@ -1,8 +0,0 @@
-export default async function handler(request, response) {
-    if (request.method == 'GET') {
-        return response.status(200).send({ message: 'GET success' });
-    } else {
-        let res = new Response();
-        return res.status(200).send({ message: 'POST success' });
-    }
-}
