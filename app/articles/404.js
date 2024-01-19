f52415d6556e8eff62786262b9c9ec6fc95da994
tokokitabutsuri <136794662+tokokitabutsuri@users.noreply.github.com>
2024-01-20 03:31:14 +0900

    add 404

---
+++ app/articles/404.js
@@ -0,0 +1,20 @@
+import styles from './404.module.css'
+export default function(){
+    return(
+        <>
+        <title>ページないよ(泣)-所北物理部</title>
+        <div style={{backgroundColor:'#fed700',height:'100vh',width:'100vw'}}>
+        <div style={{height: '1em', textAlign: 'left',display: 'block'}}>404 Not Found</div>
+    <div className={styles.wrapper} style={{height: 'calc(100vh-1em)'}}>
+        <div className={styles.content} style={{height: 'fit-content',width: 'fit-content'}}>
+            <h1 className={styles.h1}>
+                <nobr>ページないよ(泣)</nobr>
+            </h1>
+            <div className={styles.image}><img src="https://tkbutsuribu.vercel.app/ms404.JPG" alt="404姉さん" width="256" height="256" /></div>
+            <a href="#" onclick="window.history.back(); return false;">戻る</a>
+        </div>
+    </div>
+    </div>
+    </>
+    )
+}
