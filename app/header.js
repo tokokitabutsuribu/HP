d3c1554ece57bd6bc79c51216337401578d3475f
tokokitabutsuri <136794662+tokokitabutsuri@users.noreply.github.com>
2024-01-20 00:34:49 +0900

    fix

--- app/header.js
+++ app/header.js
@@ -2,9 +2,9 @@ import styles from './header.module.css'
 export default async function Header() {
     return (
         <div className={styles.wrapper}>
-            <nav className={styles.global-navi}>
+            <nav className={styles.globalNavi}>
                 <nobr><span className={styles.toplink}><a href="https://tkbutsuribu.vercel.app/">
-                    <img src="/icon-48x48.png" width="48" height="48" /><span>所北物理部</span></a></span></nobr>
+                    <img className={styles.topimg} src="/icon-48x48.png" width="48" height="48" /><span>所北物理部</span></a></span></nobr>
                 <ul className={styles.menu}>
                     <li>
                         <nobr><a href="https://tkbutsuribu.vercel.app/about.html">概要</a></nobr>
