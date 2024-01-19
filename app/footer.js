d1ff6f93f38d1a1cc5f300abbe98e11f18a8eaec
所北物理部 <136794662+tokokitabutsuri@users.noreply.github.com>
2024-01-19 10:33:12 +0900

    fix header and footer to async

--- app/footer.js
+++ app/footer.js
@@ -1,4 +1,4 @@
-export default function Footer() {
+export default async function Footer() {
     await fetch('/footer.html', { next: { revalidate: 20 } })
         .then(res => res.text())
         .then(text => new DOMParser().parseFromString(text, "text/html").body.innerHTML)
