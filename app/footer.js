b835c5e5278c70d66a9d390c5f10eeba8ae6b287
所北物理部 <136794662+tokokitabutsuri@users.noreply.github.com>
2024-01-19 11:40:48 +0900

    update fetch to write

--- app/footer.js
+++ app/footer.js
@@ -1,10 +1,39 @@
-import { headers } from 'next/headers'
-import { parse } from 'node-html-parser';
-
 export default async function Footer() {
-    const host = headers().get("host");
-    return await fetch(`https://${host}/footer.html`, { next: { revalidate: 20 } })
-        .then(res => res.text())
-        .then(text => parse(text).querySelector('body').innerHTML)
-        //.then(text=>parse(text))
+    return (
+        <footer>
+            <div className={styles.links}>
+                <div className={styles.twitter}>
+                    <a style="text-decoration: none;"
+                        href="https://twitter.com/intent/follow?ref_src=twsrc%5Etfw%7Ctwcamp%5Ebuttonembed%7Ctwterm%5Efollow%7Ctwgr%5ETK_physics_club&region=follow_link&screen_name=TK_physics_club"
+                        target="_blank" rel="noopener norefereer"><span>
+                            <nobr>𝕏 フォローする</nobr>
+                        </span></a>
+                </div>
+                <div className={styles.youtube}>
+                    <a href="https://www.youtube.com/channel/UCMx0SnqChPhp4rapoqgan3g?sub_confirmation=1" target="_blank"
+                        rel="noopener norefereer"><span>
+                            <nobr>公式YouTube</nobr>
+                        </span></a>
+                </div>
+                <div className={styles.tokokitahp}>
+                    <a href="https://tokokita-h.spec.ed.jp/" target="_blank" rel="noopener norefereer">
+                        <nobr>所北HP</nobr>
+                    </a>
+                </div>
+                <div className={styles.github}>
+                    <a href="https://github.com/tokokitabutsuri/HP" target="_blank" rel="noopener norefereer">
+                        <nobr><img height="20px" src="/github-mark-white.svg" style="margin-right: 3px;" alt=""></img><img
+                            height="22px" src="/GitHub_Logo_White.png" alt="Github"></img></nobr>
+                    </a>
+                </div>
+            </div>
+            <div className={styles.footermap}>
+                <a href="https://tkbutsuribu.vercel.app/index.html">トップページ</a><br />
+                <a href="https://tkbutsuribu.vercel.app/about.html">概要</a><br />
+                <a href="https://tkbutsuribu.vercel.app/articles/index.html">記事一覧</a><br />
+                <a href="https://tkbutsuribu.vercel.app/games/index.html">ゲームスタジオ</a><br />
+                <a href="https://tkbutsuribu.vercel.app/bbs/404.html">掲示板(未完成)</a>
+            </div>
+        </footer>
+    )
 }
--- app/header.js
+++ app/header.js
@@ -1,10 +1,25 @@
-import { headers } from 'next/headers'
-import { parse } from 'node-html-parser';
-
 export default async function Header() {
-    const host = headers().get("host");
-    return await fetch(`https://${host}/header.html`, { next: { revalidate: 20 } })
-        .then(res => res.text())
-        .then(text => parse(text).querySelector('body').innerHTML)
-        .then(text=>parse(text))
+    return (
+        <div id="wrapper" style="height: 88px">
+            <nav id="global-navi">
+                <nobr><span className={styles.toplink}><a href="https://tkbutsuribu.vercel.app/">
+                    <img src="/icon-48x48.png" width="48" height="48"
+                        style="margin-top: 5px;" /><span>所北物理部</span></a></span></nobr>
+                <ul className={styles.menu}>
+                    <li>
+                        <nobr><a href="https://tkbutsuribu.vercel.app/about.html">概要</a></nobr>
+                    </li>
+                    <li>
+                        <nobr><a href="https://tkbutsuribu.vercel.app/articles/index.html">記事</a></nobr>
+                    </li>
+                    <li>
+                        <nobr><a href="https://tkbutsuribu.vercel.app/games/index.html">スタジオ</a></nobr>
+                    </li>
+                    <li>
+                        <nobr><a href="https://tkbutsuribu.vercel.app/bbs/404.html">掲示板(未完成)</a></nobr>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )
 }
