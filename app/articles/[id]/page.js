a49e6990d55fee6fdafed0136555440aca7c7ee0
tokokitabutsuri <136794662+tokokitabutsuri@users.noreply.github.com>
2024-01-20 03:41:42 +0900

    add fetch

--- app/articles/[id]/page.js
+++ app/articles/[id]/page.js
@@ -1,14 +1,36 @@
-import Link from 'next/link'
-export default function Page({ params }) {
-  let a = "/articles/" + (params.id + 1) + "/";
+export default async function Page({ params }) {
+  let category=[];
+  let title="title";
+  let updated="----/--/--";
+  let index=[];
+  let content=[]
+
+  await fetch('https://tkbutsuribu.microcms.io/api/v1/articles/c9uqfteg5yg', { headers: { 'X-MICROCMS-API-KEY': '41k5Ew3OXIRepVdY8CgIXiAwTNwJiS5mQFpa' }, next: { revalidate: 10 } })
+    .then(data => data.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((e)=>{
+    return { notFound: true };
+  })
   return (
     <div>
-      <hr></hr>
-      <div>My Post</div>
-      <div>{params.id}</div>
-      <a href={a}>a</a><br />
-      <Link href={a}>Link</Link>
-      <hr></hr>
+      <ul id="category">
+      </ul>
+      <h1 id="title"></h1>
+      <p id="updated"></p>
+      <ol id="index">
+      </ol>
+      <div id="content"></div>
+      <br />
+      <div id="top-wrapper">
+        <a id="top" href="javascript:window.location.replace('#.top')">
+          <div>
+            <span>
+              ↑<br />TOP</span>
+          </div>
+        </a>
       </div>
-  )
+    </div>
+  );
 }
