8bbcf68cce763a3a3696386e018d3dc52008c99e
tokokitabutsuri <136794662+tokokitabutsuri@users.noreply.github.com>
2024-01-21 01:21:28 +0900

    fix

--- app/articles/404.js
+++ app/articles/404.js
@@ -3,7 +3,7 @@ export default function(){
     return(
         <>
         <title>ページないよ(泣)-所北物理部</title>
-        <div style={{backgroundColor:'#fed700',height:'100vh',width:'100vw',position:'static',top:'0',bottom:'0',right:'0',left:'0',zIndex:'10000'}}>
+        <div style={{backgroundColor:'#fed700',height:'100vh',width:'100vw',position:'fixed',top:'0',bottom:'0',right:'0',left:'0',zIndex:'10000'}}>
         <div style={{height: '1em', textAlign: 'left',display: 'block'}}>404 Not Found</div>
     <div className={styles.wrapper} style={{height: 'calc(100vh-1em)'}}>
         <div className={styles.content} style={{height: 'fit-content',width: 'fit-content'}}>
