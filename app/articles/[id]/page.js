4078ffa729b8fd912411a1a972ec00e007c74782
tokokitabutsuri <136794662+tokokitabutsuri@users.noreply.github.com>
2024-01-22 18:19:55 +0900

    update

--- app/articles/[id]/page.js
+++ app/articles/[id]/page.js
@@ -6,6 +6,44 @@ export default async function Page({ params }) {
   let index = [];
   let content = [];
 
+  const replacecontent = (input) => {
+    const replacekeys = [
+      { key: '<table>', replace: '<div class="table-wrapper"><table>' },
+      { key: '</table>', replace: '</table></div>' },
+      { key: '<iframe', replace: '<iframe loading="lazy"' },
+    ]
+    let ret = input;
+    for (const replacekey of replacekeys) {
+      ret = ret.replaceAll(replacekey.key, replacekey.replace);
+    }
+    return ret;
+  }
+
+  const addarray = (array, tag, key) => {
+    let add = "";
+    let starttag;
+    let endtag;
+    if (tag !== undefined) {
+      starttag = '<' + tag + '>'
+      endtag = '</' + tag + '>\n'
+    } else {
+      starttag = ''
+      endtag = ''
+    }
+    if (!array.lenth) {
+      if (key === undefined) {
+        for (const elem of array) {
+          add += starttag + replacecontent(elem) + endtag;
+        }
+      } else {
+        for (const elem of array) {
+          add += starttag + replacecontent(elem[key]) + endtag;
+        }
+      }
+    }
+    return add;
+  }
+
   let iserror = false;
   await fetch('https://tkbutsuribu.microcms.io/api/v1/articles/' + params.id, { headers: { 'X-MICROCMS-API-KEY': '41k5Ew3OXIRepVdY8CgIXiAwTNwJiS5mQFpa' }, next: { revalidate: 10 } })
     .then(data => {
@@ -14,8 +52,15 @@ export default async function Page({ params }) {
       }
       return data.json();
     })
-    .then((data) => {
-      console.log(data);
+    .then((res) => {
+      console.log(res);
+      title = res.title;
+      updated = new Date(res.revisedAt).toISOString().split("T")[0].replaceAll("-", "/");
+      content = addarray(res.contents, undefined, 'content');
+      category = addarray(res.category, 'li', undefined);
+      for (const elem of res.index) {
+        index += '<li><a href="javascript:window.location.replace(\'#' + elem.link + '\')">' + elem.index + '</a></li>\n';
+      }
     })
     .catch((e) => {
       iserror = true;
@@ -27,12 +72,14 @@ export default async function Page({ params }) {
   return (
     <div>
       <ul id="category">
+        {category}
       </ul>
-      <h1 id="title"></h1>
-      <p id="updated"></p>
+      <h1 id="title">{title}</h1>
+      <p id="updated">{updated}</p>
       <ol id="index">
+        {index}
       </ol>
-      <div id="content"></div>
+      <div id="content">{content}</div>
       <br />
       <div id="top-wrapper">
         <a id="top" href="javascript:window.location.replace('#.top')">
