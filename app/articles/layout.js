e424534ad3088555093565aec04281243946f1b7
所北物理部 <136794662+tokokitabutsuri@users.noreply.github.com>
2024-01-23 02:03:15 +0900

    add prism

--- app/articles/layout.js
+++ app/articles/layout.js
@@ -1,11 +1,17 @@
 import './article.css'
+import { useEffect } from 'react'
+import Prism from './prism.js'
+import './prism.css'
 
 export default function RootLayout({
   children,
 }) {
+  useEffect(() => {
+    Prism.highlightAll();
+  })
   return (
-    <div class="main line-numbers" style={{textAlign:'left'}}>
-        {children}
+    <div class="main line-numbers" style={{ textAlign: 'left' }}>
+      {children}
     </div>
   );
 }
