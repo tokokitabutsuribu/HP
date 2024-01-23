b55e1efbaf4126b269ae88d64e8e0f33d6f90fdc
tokokitabutsuri <136794662+tokokitabutsuri@users.noreply.github.com>
2024-01-23 00:13:53 +0000

    fix myprism

--- app/articles/layout.js
+++ app/articles/layout.js
@@ -1,14 +1,14 @@
 import './article.css'
 import './prism.css'
-import MyPrism from './myprism.js'
+import {myprism} from './myprism.js'
 
 export default function RootLayout({
   children,
 }) {
+  myprism();
   return (
     <div class="main line-numbers" style={{ textAlign: 'left' }}>
       {children}
-      <MyPrism />
     </div>
   );
 }
