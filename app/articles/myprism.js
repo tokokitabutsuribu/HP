f1982d365e822b3626bbc88c0a9f7f5ddd979c12
所北物理部 <136794662+tokokitabutsuri@users.noreply.github.com>
2024-01-23 10:31:08 +0900

    remove test

--- app/articles/myprism.js
+++ app/articles/myprism.js
@@ -1,14 +1,12 @@
 "use client"
 
 import { useEffect } from 'react'
-//import Prism from './prism.js'
+import Prism from './prism.js'
 
 export default function myprism() {
-  import Prism2 from '/articles/prism.js'
   useEffect(() => {
     try {
-      Prism2.highlightAll();
-      //Prism.highlightAll();
+      Prism.highlightAll();
     } catch (e) {
       window.alert(e);
     }
