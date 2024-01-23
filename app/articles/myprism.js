ddc7002988aee03c88aa5727ea450b4158be02e0
所北物理部 <136794662+tokokitabutsuri@users.noreply.github.com>
2024-01-23 12:55:32 +0900

    test

--- app/articles/myprism.js
+++ app/articles/myprism.js
@@ -11,5 +11,9 @@ export default function myprism() {
       window.alert(e);
     }
   })
-  return <span></span>
+  return (<span>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="https://tkbutsuribu.vercel.app/jquery.arbitrary-anchor.js"></script>    
+  </span>)
 }
