7fe5e267c2f9c8ce76e1ffd147ab3de17a6735f9
所北物理部 <136794662+tokokitabutsuri@users.noreply.github.com>
2024-01-26 12:46:40 +0900

    fix

--- app/articles/[id]/[draft]/page.js
+++ app/articles/[id]/[draft]/page.js
@@ -1,5 +1,5 @@
 import { Metadata } from 'next'
-import { meta, articlePage } from '../article.js'
+import { meta, articlePage } from '../../article.js'
 
 export async function generateMetadata({ params }) {
   return await meta('https://tkbutsuribu.microcms.io/api/v1/articles/' + params.id + '?draftKey=' + params.draft, 0)
