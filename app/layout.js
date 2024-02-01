7720fa94ecbcf95b701dbf3a35a94ae0c5285bbe
所北物理部 <136794662+tokokitabutsuri@users.noreply.github.com>
2024-02-01 12:48:46 +0900

    add GTM

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,15 @@ export default function RootLayout({
 
       </head>
       <body>
+      <script dangerouslySetInnerHTML={{
+      __html:`<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TVKWHB4T');</script>
+<!-- End Google Tag Manager -->`,}} />
+
         {(process.env.NODE_ENV === "development" || process.env.VERCEL_ENV === "preview") && (
           // eslint-disable-next-line @next/next/no-sync-scripts
           <script
@@ -27,7 +36,11 @@ export default function RootLayout({
 
       gtag('config', 'G-0SBGM4VKQL');
     `,}} />
-          
+          <script dangerouslySetInnerHTML={{
+      __html:`<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TVKWHB4T"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->`,}} />
         <Header />
         {children}
         <Footer />
