29a88117f4d0c8f876e8d551393a4c008ac0d6f2
所北物理部 <136794662+tokokitabutsuri@users.noreply.github.com>
2024-02-01 22:46:46 +0900

    delete GA

--- app/layout.js
+++ app/layout.js
@@ -27,15 +27,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             src="https://snippet.meticulous.ai/v1/meticulous.js"
           />
         )}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-0SBGM4VKQL"></script>
-        <script dangerouslySetInnerHTML={{
-          __html: `window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-
-      gtag('config', 'G-0SBGM4VKQL');
-    `,
-        }} />
         <script dangerouslySetInnerHTML={{
           __html: `<!-- Google Tag Manager (noscript) -->
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TVKWHB4T"
