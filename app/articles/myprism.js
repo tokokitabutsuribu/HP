800fcde1d2beb454659e36e26da827b194bcc66c
tokokitabutsuri <136794662+tokokitabutsuri@users.noreply.github.com>
2024-02-06 03:40:10 +0900

    fix

--- app/articles/myprism.js
+++ app/articles/myprism.js
@@ -1,15 +1,20 @@
-"use client"
-import Prism from 'prismjs'
+"use client";
+import Prism from 'prismjs';
 
 export default function myprism() {
-    try {
-      Prism.highlightAll();
-    } catch (e) {
-      console.log(e);
-    }
-  return (<span>
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
-        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous" />
-    <script type="text/javascript" src="https://tkbutsuribu.vercel.app/jquery.arbitrary-anchor.js" />    
-  </span>)
+	try {
+		Prism.highlightAll();
+	} catch (e) {
+		console.log(e);
+	}
+	return (<span>
+		<script src="https://code.jquery.com/jquery-3.7.1.min.js"
+			integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous" />
+		<script type="text/javascript" src="https://tkbutsuribu.vercel.app/jquery.arbitrary-anchor.js" />
+		<script>
+			window.onload({
+				Prism.highlightAll()
+			})
+		</script>
+	</span>);
 }
