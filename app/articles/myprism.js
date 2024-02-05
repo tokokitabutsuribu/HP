a11b9c09ee6a170577da295efccd60e8cb8a9c72
tokokitabutsuri <136794662+tokokitabutsuri@users.noreply.github.com>
2024-02-06 03:44:05 +0900

    fix

--- app/articles/myprism.js
+++ app/articles/myprism.js
@@ -3,13 +3,13 @@ import Prism from 'prismjs';
 import { useEffect } from 'react';
 
 export default function myprism() {
-	useEffect(()=>{
+	useEffect(() => {
 		try {
 			Prism.highlightAll();
 		} catch (e) {
 			console.log(e);
 		}
-	})
+	});
 
 	return (<span>
 		<script src="https://code.jquery.com/jquery-3.7.1.min.js"
