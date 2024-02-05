757e7eabb3b9b5763a70ec4fa829118612313a2f
tokokitabutsuri <136794662+tokokitabutsuri@users.noreply.github.com>
2024-02-06 03:40:34 +0900

    fix

--- app/articles/myprism.js
+++ app/articles/myprism.js
@@ -2,11 +2,11 @@
 import Prism from 'prismjs';
 
 export default function myprism() {
-	try {
-		Prism.highlightAll();
-	} catch (e) {
-		console.log(e);
-	}
+	// try {
+	// 	Prism.highlightAll();
+	// } catch (e) {
+	// 	console.log(e);
+	// }
 	return (<span>
 		<script src="https://code.jquery.com/jquery-3.7.1.min.js"
 			integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous" />
