51c1842bf625d07074ddc3e2c0ae6e9767179ec0
tokokitabutsuri <136794662+tokokitabutsuri@users.noreply.github.com>
2024-02-06 03:43:56 +0900

    fix

--- app/articles/myprism.js
+++ app/articles/myprism.js
@@ -1,20 +1,19 @@
 "use client";
 import Prism from 'prismjs';
+import { useEffect } from 'react';
 
 export default function myprism() {
-	// try {
-	// 	Prism.highlightAll();
-	// } catch (e) {
-	// 	console.log(e);
-	// }
+	useEffect(()=>{
+		try {
+			Prism.highlightAll();
+		} catch (e) {
+			console.log(e);
+		}
+	})
+
 	return (<span>
 		<script src="https://code.jquery.com/jquery-3.7.1.min.js"
 			integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous" />
 		<script type="text/javascript" src="https://tkbutsuribu.vercel.app/jquery.arbitrary-anchor.js" />
-		<script>
-			window.onload({
-				Prism.highlightAll()
-			})
-		</script>
 	</span>);
 }
