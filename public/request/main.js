61aafbd94182afa84ee91766ea398dc990ecac6d
所北物理部 <136794662+tokokitabutsuri@users.noreply.github.com>
2024-02-20 01:46:43 +0900

    fix

--- public/request/main.js
+++ public/request/main.js
@@ -7,7 +7,7 @@ try {
 		num++;
 		music.id = num;
 		music.innerHTML = `<lavel">曲名<input type="text" name="musicname" placeholder="例:荒城の月" /></lavel>
-    <label>作曲者<input type="te/xt" name="artistname" placeholder="例:滝廉太郎" /></label>
+    <label>作曲者<input type="text" name="artistname" placeholder="例:滝廉太郎" /></label>
 	<button class="remove" type="button" onclick="document.getElementById('${num}').remove()">-</button>`;
 
 		document.getElementById('musiclist').appendChild(music);
