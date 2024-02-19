0ee395248fef8e0f2b009abef4bf2a21d981d39b
所北物理部 <136794662+tokokitabutsuri@users.noreply.github.com>
2024-02-19 18:11:10 +0900

    fix

--- public/request/main.js
+++ public/request/main.js
@@ -1,6 +1,10 @@
-document.getElementById('add').onclick(()=>{
+const addbutton = document.getElementById('add')
+addbutton.onclick(()=>{
     const musicname = document.createElement('label')
     const artistname = document.createElement('label')
+    
     musicname.innerHTML='曲名<input type="text" name="musicname" />'
     artistname.innerHTML='作曲者<input type="te/xt" name="artistname" />'
+
+    addbutton.before([musicname,artistname])
 })
