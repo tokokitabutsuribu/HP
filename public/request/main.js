c9edd5a6096952abc5cd58eb8f28ab73b6b99925
所北物理部 <136794662+tokokitabutsuri@users.noreply.github.com>
2024-02-19 18:07:37 +0900

    add request

---
+++ public/request/main.js
@@ -0,0 +1,6 @@
+document.getElementById('add').onclick(()=>{
+    const musicname = document.createElement('label')
+    const artistname = document.createElement('label')
+    musicname.innerHTML='曲名<input type="text" name="musicname" />'
+    artistname.innerHTML='作曲者<input type="te/xt" name="artistname" />'
+})
