1e47d19e3198b5018f5cfa0ad24266f727feb5aa
tokokitabutsuri <136794662+tokokitabutsuri@users.noreply.github.com>
2024-02-27 01:28:32 +0900

    fix

--- app/NotifyButton.tsx
+++ app/NotifyButton.tsx
@@ -14,112 +14,113 @@ const firebaseConfig = {
 	appId: "1:841487077695:web:42229ff499ed5c91d0838a",
 	measurementId: "G-G1N7SE550S",
 };
-const app = initializeApp(firebaseConfig);
-const messaging = getMessaging(app);
-const APIURL = "/api/push_token";
-async function getmytoken() {
-	let iserror = false;
-	getToken(messaging, { vapidKey: "BHYfDERRzVeOZOz32LOi6uZTYpzItJ5MVK8EswEeYkjLLOeX8thI1o7yPBuizxXqq_j_r1pauCAo3_YTGWxc7tQ" })
-		.then((currentToken) => {
-			if (currentToken) {
-				console.log(currentToken);
-				localStorage.messageToken = currentToken;
-				document.getElementById("requestpermission").style.display = "none";
-				fetch(APIURL, {
-					method: "POST",
-					headers: {
-						"Content-Type": "application/json",
-					},
-					body: JSON.stringify({
-						token: currentToken,
-						true_topics: ["all"],
-						false_topics: [],
-					}),
-				})
-					.then((res) => {
-						console.log(res);
-					})
-					.catch((error) => {
-						console.log(error);
-						iserror = true;
-					});
-			} else {
-				// Show permission request UI
-				console.log("No registration token available. Request permission to generate one.");
-				document.getElementById("requestpermission").style.display = "block";
-				// ...
-			}
-		})
-		.catch((err) => {
-			console.log("An error occurred while retrieving token. ", err);
-			document.getElementById("requestpermission").style.display = "block";
-			// ...
-		});
-}
-function registerInstallAppEvent(elem) {
-	//インストールバナー表示条件満足時のイベントを乗っ取る
-	window.addEventListener("beforeinstallprompt", (event) => {
-		console.log("beforeinstallprompt: ", event);
-		event.preventDefault(); //バナー表示をキャンセル
-		elem.promptEvent = event; //eventを保持しておく
-		elem.style.display = "block"; //要素を表示する
-		return false;
-	});
-	//インストールダイアログの表示処理
-	function installApp() {
-		if (elem.promptEvent) {
-			elem.promptEvent.prompt(); //ダイアログ表示
-			elem.promptEvent.userChoice.then((choice) => {
-				elem.style.display = "none";
-				elem.promptEvent = null; //一度しか使えないため後始末
-			}); //end then
-		}
-	} //end installApp
-	//ダイアログ表示を行うイベントを追加
-	elem.addEventListener("click", installApp);
-}
 
 export default function () {
-	document.getElementById("requestpermission").onclick = function requestPermission() {
-		console.log("Requesting permission...");
-		const ua = navigator.userAgent;
-		if (/iPad|iPhone|iPod/.test(ua) || (/Macintosh/.test(ua) && (navigator.maxTouchPoints > 1 || "ontouchend" in document)) || ((ua.indexOf("ipad") > -1 || ua.indexOf("Macintosh") > -1) && "ontouchend" in document)) {
-			if (!window.matchMedia("(display-mode: standalone)").matches) {
-				(document.getElementById("pop-up") as HTMLInputElement).checked = true;
-			}
+	useEffect(() => {
+		const app = initializeApp(firebaseConfig);
+		const messaging = getMessaging(app);
+		const APIURL = "/api/push_token";
+		async function getmytoken() {
+			let iserror = false;
+			getToken(messaging, { vapidKey: "BHYfDERRzVeOZOz32LOi6uZTYpzItJ5MVK8EswEeYkjLLOeX8thI1o7yPBuizxXqq_j_r1pauCAo3_YTGWxc7tQ" })
+				.then((currentToken) => {
+					if (currentToken) {
+						console.log(currentToken);
+						localStorage.messageToken = currentToken;
+						document.getElementById("requestpermission").style.display = "none";
+						fetch(APIURL, {
+							method: "POST",
+							headers: {
+								"Content-Type": "application/json",
+							},
+							body: JSON.stringify({
+								token: currentToken,
+								true_topics: ["all"],
+								false_topics: [],
+							}),
+						})
+							.then((res) => {
+								console.log(res);
+							})
+							.catch((error) => {
+								console.log(error);
+								iserror = true;
+							});
+					} else {
+						// Show permission request UI
+						console.log("No registration token available. Request permission to generate one.");
+						document.getElementById("requestpermission").style.display = "block";
+						// ...
+					}
+				})
+				.catch((err) => {
+					console.log("An error occurred while retrieving token. ", err);
+					document.getElementById("requestpermission").style.display = "block";
+					// ...
+				});
 		}
-		Notification.requestPermission()
-			.then((permission) => {
-				if (permission === "granted") {
-					console.log("Notification permission granted.");
-					document.getElementById("requestpermission").style.display = "none";
-					getmytoken();
-				} else {
-					const ua = navigator.userAgent;
-					if (/iPad|iPhone|iPod/.test(ua) || (/macintosh/.test(ua) && (navigator.maxTouchPoints > 1 || "ontouchend" in document)) || ua.indexOf("ipad") > -1 || (ua.indexOf("macintosh") > -1 && "ontouchend" in document)) {
-						if (!window.matchMedia("(display-mode: standalone)").matches) {
-							(document.getElementById("pop-up") as HTMLInputElement).checked = true;
+		function registerInstallAppEvent(elem) {
+			//インストールバナー表示条件満足時のイベントを乗っ取る
+			window.addEventListener("beforeinstallprompt", (event) => {
+				console.log("beforeinstallprompt: ", event);
+				event.preventDefault(); //バナー表示をキャンセル
+				elem.promptEvent = event; //eventを保持しておく
+				elem.style.display = "block"; //要素を表示する
+				return false;
+			});
+			//インストールダイアログの表示処理
+			function installApp() {
+				if (elem.promptEvent) {
+					elem.promptEvent.prompt(); //ダイアログ表示
+					elem.promptEvent.userChoice.then((choice) => {
+						elem.style.display = "none";
+						elem.promptEvent = null; //一度しか使えないため後始末
+					}); //end then
+				}
+			} //end installApp
+			//ダイアログ表示を行うイベントを追加
+			elem.addEventListener("click", installApp);
+		}
+
+		const ua = navigator.userAgent;
+		document.getElementById("requestpermission").onclick = function requestPermission() {
+			console.log("Requesting permission...");
+
+			if (/iPad|iPhone|iPod/.test(ua) || (/Macintosh/.test(ua) && (navigator.maxTouchPoints > 1 || "ontouchend" in document)) || ((ua.indexOf("ipad") > -1 || ua.indexOf("Macintosh") > -1) && "ontouchend" in document)) {
+				if (!window.matchMedia("(display-mode: standalone)").matches) {
+					(document.getElementById("pop-up") as HTMLInputElement).checked = true;
+				}
+			}
+			Notification.requestPermission()
+				.then((permission) => {
+					if (permission === "granted") {
+						console.log("Notification permission granted.");
+						document.getElementById("requestpermission").style.display = "none";
+						getmytoken();
+					} else {
+						const ua = navigator.userAgent;
+						if (/iPad|iPhone|iPod/.test(ua) || (/macintosh/.test(ua) && (navigator.maxTouchPoints > 1 || "ontouchend" in document)) || ua.indexOf("ipad") > -1 || (ua.indexOf("macintosh") > -1 && "ontouchend" in document)) {
+							if (!window.matchMedia("(display-mode: standalone)").matches) {
+								(document.getElementById("pop-up") as HTMLInputElement).checked = true;
+							} else {
+								document.getElementById("requestpermission").innerText = "通知がブロックされています";
+							}
 						} else {
 							document.getElementById("requestpermission").innerText = "通知がブロックされています";
 						}
-					} else {
-						document.getElementById("requestpermission").innerText = "通知がブロックされています";
 					}
-				}
-			})
-			.catch((error) => {
-				console.log(error);
-			});
-	};
+				})
+				.catch((error) => {
+					console.log(error);
+				});
+		};
 
-	useEffect(() => {
-		const ua = navigator.userAgent;
 		if (/iPad|iPhone|iPod/.test(ua) || (/macintosh/.test(ua) && (navigator.maxTouchPoints > 1 || "ontouchend" in document)) || ua.indexOf("ipad") > -1 || (ua.indexOf("macintosh") > -1 && "ontouchend" in document)) {
-			document.getElementById("InstallBtn").addEventListener('click',()=>{
+			document.getElementById("InstallBtn").addEventListener("click", () => {
 				(document.getElementById("pop-up") as HTMLInputElement).checked = true;
-			})
-		}else{
-			registerInstallAppEvent(document.getElementById("InstallBtn"))
+			});
+		} else {
+			registerInstallAppEvent(document.getElementById("InstallBtn"));
 		}
 		if (window.matchMedia("(display-mode: standalone)").matches) {
 			document.getElementById("InstallBtn").style.display = "none";
