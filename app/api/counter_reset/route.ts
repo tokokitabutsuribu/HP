9950ba8e06c0ac43cbed8899c8475cc62235322b
tokokitabutsuri <136794662+tokokitabutsuri@users.noreply.github.com>
2024-02-29 02:59:39 +0900

    add counterreset

--- app/api/counter_reset/route.ts
+++ app/api/counter_reset/route.ts
@@ -1,5 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
+import { kv } from "@vercel/kv";
 
 export async function POST(request:NextRequest){
+    const now = new Date().getUTCHours();
+    await kv.del('users');
+    await kv.lpush('last_reset',now)
     return NextResponse.json({})
 }
