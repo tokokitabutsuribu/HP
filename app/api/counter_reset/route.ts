26a8b9fdcb99edc049cec154853d838adda211ba
tokokitabutsuri <136794662+tokokitabutsuri@users.noreply.github.com>
2024-03-01 21:10:12 +0900

    add github action

--- app/api/counter_reset/route.ts
+++ app/api/counter_reset/route.ts
@@ -2,6 +2,13 @@ import { NextRequest, NextResponse } from "next/server";
 import { kv } from "@vercel/kv";
 
 export async function POST(request:NextRequest){
+    const body=await request.json()
+    if(body.Auth!==process.env.COUNTER_SECRET){
+        console.warn('bad access')
+        console.log(`Auth value:${body.Auth}`)
+        return NextResponse.json({},{status:403,statusText:'Forbidden'})
+    }
+
     const now = new Date().getUTCHours();
     await kv.del('users');
     await kv.lpush('last_reset',now)
