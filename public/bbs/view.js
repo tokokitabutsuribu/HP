c4458c7e636e4cac4ca1ce32593c857c25bda011
tokokitabutsuri <136794662+tokokitabutsuri@users.noreply.github.com>
2024-03-07 02:32:19 +0900

    update

---
+++ public/bbs/view.js
@@ -0,0 +1,43 @@
+
+
+
+
+
+
+function post() {
+	const data = {
+		name: document.getElementById('name').value,
+		message: document.getElementById('message').value
+	};
+	alert(JSON.stringify(data));
+}
+
+window.addEventListener('click', () => {
+	const menu = Array.from(document.getElementsByClassName('menuon'));
+	for (const i in menu) {
+		menu[i].classList.remove('menuon');
+	}
+
+});
+
+const menubuttonlist = Array.from(document.getElementsByClassName('menubutton'));
+for (const i in menubuttonlist) {
+	menubuttonlist[i].addEventListener('click', (e) => {
+		menubuttonlist[i].parentElement.classList.toggle('menuon');
+		e.stopPropagation();
+	});
+}
+
+const replacemessage = (message = '') => {
+	let m = '';
+	m = message.replace(/[&'"]/g, m => ({
+		"&": "&amp;",
+		"'": "&apos;",
+		'"': '&quot;'
+	})[m]);
+	m = m.replaceAll('<', '&lt;');
+	m = m.replaceAll(/>>([0-9]+)/g, '<$1');
+	m = m.replaceAll('>', '&gt;');
+	m = m.replaceAll(/<([0-9]+)/g, '<a href="#$1">&gt;&gt;$1</a>');
+	return m;
+};
